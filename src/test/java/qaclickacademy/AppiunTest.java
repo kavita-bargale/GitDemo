2842b833cacdea43cffe3df227250c6a571800fb
kavita-bargale <kavita.bargale@gmail.com>
2019-03-18 18:58:03 +0530

    Architect Asian Development

--- src/test/java/qaclickacademy/AppiunTest.java
+++ src/test/java/qaclickacademy/AppiunTest.java
@@ -8,12 +8,17 @@ public class AppiunTest
 	public void AppiumTest1()
 	{
 		System.out.println("Appium test1");
+		System.out.println("Appium demo1");
+		System.out.println("Appium demo2");
+		System.out.println("Appium demo3");
 
 	}
 	@Test
 	public void AppiumTest2()
 	{
 		System.out.println("Appium test2");
+		System.out.println("Appium test3");
+		System.out.println("Appium test4");
 
 	}
 
--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -9,7 +9,7 @@ public class SeleniumTest
 	public void selenuimTest1()
 	{
 		System.out.println("selenium test1");
-
+		System.out.println("selenium test2");
 	}
 	@Test
 	public void selenuimTest2()
@@ -21,6 +21,6 @@ public class SeleniumTest
 	public void selenuimTest3()
 	{
 		System.out.println("selenium test3");
-
+		System.out.println("selenium test31");
 	}
 }
