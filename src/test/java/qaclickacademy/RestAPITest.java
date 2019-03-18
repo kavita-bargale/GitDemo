5a74f780a9381815cab1fb699185995a26ea1af2
kavita-bargale <kavita.bargale@gmail.com>
2019-03-18 18:42:03 +0530

    this is commit from American timezone guy

--- src/test/java/qaclickacademy/RestAPITest.java
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -11,7 +11,8 @@ public class RestAPITest
 		System.out.println("RestAPI test2");
 		System.out.println("RestAPI test3");
 		System.out.println("RestAPI test4");
-
+		System.out.println("RestAPI test5");
+		System.out.println("RestAPI test6");
 	}
 	
 	@Test
